57ac0bbdf46f108025da66322e70775732c5169a
Rabieh-S <sassi.rabieh@gmail.com>
2023-07-27 19:57:18 +0200

    feat: added Patch and Delete HTTP Request

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,26 @@ import express from 'express';
 import { get, merge } from 'lodash';
 import { getUserBySessionToken } from '../db/users';
 
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, 'identity._id') as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
     const sessionToken = req.cookies['RABIEH-AUTH'];
@@ -17,7 +37,7 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
     }
 
     merge(req, { identity: existingUser });
-
+    return next();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { isAuthenticated, isOwner } from '../middlewares';
 
-import { getAllUsers } from '../controllers/users';
+
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 
 export default (router: express.Router) => {
-  router.get('/users', getAllUsers);
+  router.get('/users', isAuthenticated, getAllUsers);
+  router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
+  router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 }
